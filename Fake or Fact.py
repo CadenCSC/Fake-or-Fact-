da8eb486a60fbefe98cf28c9d6bef58098f55873
Caden Read <22095@nayland.school.nz>
2025-05-08 15:12:43 +1200

    Fake or fact

--- Fake or Fact.py
+++ Fake or Fact.py
@@ -7,12 +7,16 @@ root.geometry('1350x800')
 lbl = tk.Label(root, text = "Fake or Fact")
 lbl.grid()
 
-def clicked():
+def fakeclicked():
     lbl.configure(text= "FAKE")
 
-btn = tk.Button(root, text = "FAKE" ,
-                fg = "red", command=clicked)
+def factclicked():
+    lbl.configure(text= "FACT")
+    
+btnfake = tk.Button(root, text = "FAKE" , bg = "red", fg = "White", padx = 200, pady = 40, command=fakeclicked)
+btnfake.place(x = 40, y = 650)
 
-btn.grid(column=2, row=10)
+btnfact = tk.Button(root, text = "FACT", bg = "green", fg = "White", padx=  200, pady = 40, command=factclicked)
+btnfact.place(x = 875, y =650)
 
 root.mainloop()
